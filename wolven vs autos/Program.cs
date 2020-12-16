0782652cb7ab25fb334e46906513f1f05d3b1fab
Thierry <Tdwolffenbuttel@gmail.com>
2020-12-16 18:58:05 +0100

    Add project files.

---
+++ wolven vs autos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using Wolven_vs_autosML.Model;
+
+namespace wolven_vs_autos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Debug.WriteLine(" Object is not valid for this category.");
+                // Add input data
+                var input = new ModelInput();
+                Console.WriteLine("Wat is het pad van de foto?");
+                input.ImageSource = Console.ReadLine();
+
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                Debug.WriteLine(result.Prediction);
+                Console.WriteLine("De foto valt onder de catogorie:");
+                Console.WriteLine(result.Prediction);
+                Console.WriteLine("wil je nog een foto testen?");
+            } while (Console.ReadLine().ToLower() == "ja");
+        }
+
+    }
+}
